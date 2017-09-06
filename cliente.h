8aa59cea773b666b5aab4344993ebca45628e7d7
florencia95 <florenciaerramuspe@hotmail.com>
2017-09-06 20:32:31 -0300

    Add files via upload

---
+++ cliente.h
@@ -0,0 +1,17 @@
+//
+// Created by florencia on 03/09/17.
+//
+
+#ifndef INC_75_42_PEPAS_CLIENTE_CLIENTE_H
+#define INC_75_42_PEPAS_CLIENTE_CLIENTE_H
+
+#include <string>
+using namespace std;
+
+class Cliente{
+
+
+
+};
+
+#endif //INC_75_42_PEPAS_CLIENTE_CLIENTE_H
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "consola.h"
+using namespace std;
+
+
+int main() {
+
+    Consola consola;
+    consola.cargarPaginaPrincipal();
+
+
+}
+
+
