72707ad6009257d9d256941fdd3c2e030c73d42c
florencia95 <florenciaerramuspe@hotmail.com>
2017-09-06 17:33:23 -0300

    Add files via upload

--- PEPAS/src/com.pepas.model/cliente.cpp
+++ PEPAS/src/com.pepas.model/cliente.cpp
@@ -1,36 +1,6 @@
 #include "cliente.h"
 
-Cliente::Cliente(){
-    this->contrasenia = "";
-    this->usuario = "";
-    this->IPservidor;
-    this-> puertoServidor;
-}
 
-Cliente::Cliente(string usuario,string contrasenia){
-
-    this->usuario = usuario;
-    this->contrasenia = contrasenia;
-
-}
-
-void Cliente::setUsuario(string usuario){
-
-    this->usuario = usuario;
-}
-
-void Cliente::setContrasenia(string contrasenia){
-
-    this->contrasenia = contrasenia;
-}
-
-string Cliente::getContrasenia(){
-    return this->contrasenia;
-}
-
-string Cliente::getUsuario(){
-    return this->usuario;
-}
 
 
 
--- PEPAS/src/com.pepas.model/cliente.h
+++ PEPAS/src/com.pepas.model/cliente.h
@@ -11,26 +11,7 @@ using namespace std;
 class Cliente{
 
 
-private:
-    string usuario;
-    string contrasenia;
-    string IPservidor;
-    int puertoServidor;
 
-
-public:
-
-    Cliente();
-
-    Cliente(string usuario, string contrasenia);
-
-    void setUsuario(string usuario);
-
-    void setContrasenia(string contrasenia);
-
-    string getUsuario();
-
-    string getContrasenia();
 };
 
 #endif //INC_75_42_PEPAS_CLIENTE_CLIENTE_H
